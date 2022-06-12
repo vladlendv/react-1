81ddcf289f1c1fe9cb642fd83164210287e91eda
Vladlen Dvornikov <tubebiznes@gmail.com>
2022-06-12 08:03:27 +0500

    f c

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <header className="header">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMmAD6ddTDbrX2egxtgmOITK0lAA8EIOii_w&usqp=CAU"
+        alt="logo"
+      />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+function Profile() {
+  return (
+    <div className="content">
+      <div>
+        <img
+          height="300px;"
+          width="100%"
+          src="https://funart.pro/uploads/posts/2021-03/1617081181_10-p-oboi-panorama-lesa-14.jpg"
+          alt="content-avatar"
+        />
+      </div>
+      <div>avatar + description</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
